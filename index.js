413b0b7752e95d72ff6af84c0ff1fa9d70d559fa
hally-b <thespeedygenius@gmail.com>
2022-06-03 16:52:24 -0400

    made slider - need to make time compatible

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//for the modal
+// for the modal
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("recs");
 var span = document.getElementsByClassName("close")[0];
@@ -14,39 +14,10 @@ window.onclick = function(event) {
   }
 }
 
-$('#slider-range').slider({
-    range: 'min',
-    min: 0,
-    max: 1440,
-    step: 15,
-    value: 720,
-    animate: 'fast',
-    slide: function (e, ui) {
+var rangeslider = document.getElementById("sliderRange");
+var output = document.getElementById("val");
+output.innerHTML = rangeslider.value;
 
-        var hours1 = Math.floor(ui.value / 60);
-        var minutes1 = ui.value - (hours1 * 60);
-
-        if (hours1.length == 1) hours1 = '0' + hours1;
-        if (minutes1.length == 1) minutes1 = '0' + minutes1;
-        if (minutes1 == 0) minutes1 = '00';
-        if (hours1 >= 12) {
-            if (hours1 == 12) {
-                hours1 = hours1;
-                minutes1 = minutes1 + ' PM';
-            } else {
-                hours1 = hours1 - 12;
-                minutes1 = minutes1 + ' PM';
-            }
-        } else {
-            hours1 = hours1;
-            minutes1 = minutes1 + ' AM';
-        }
-        if (hours1 == 0) {
-            hours1 = 12;
-            minutes1 = minutes1;
-        }
-
-        $('.slider-time').html(hours1 + ':' + minutes1);
-    }
-
-});
+rangeslider.oninput = function() {
+  output.innerHTML = this.value;
+}
